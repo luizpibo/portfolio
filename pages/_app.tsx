3bd912eae694ee660257c9e18e583f8ff48ee220
Luiz Fernanado <75041533+luizpibo@users.noreply.github.com>
2022-05-23 20:15:51 -0300

    projeto base com typescript e styled components

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import type { AppProps } from "next/app";
+import GlobalStyle from "../src/theme/global";
+
+import { ThemeProvider } from "styled-components";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const theme = {
+    colors: {
+      blue: "#00D2DF",
+      gradientPink: "linear-gradient(to right, #D24074, #6518B4)",
+      gradientPurple: "linear-gradient(to right, #9358F7, #10D7E2)",
+    },
+    neutralColors: {
+      dark: "#121212",
+      "dark-200": "#151515",
+      "gray-light": "#C4C4C4",
+      light: "#FFFFFF",
+    },
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Header from "../src/pages/home/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
